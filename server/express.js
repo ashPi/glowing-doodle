9987356581ec75ecb55c1b79fbd9d5e496af57aa
ashPi <silviya_tacheva@outlook.com>
2021-03-15 21:51:21 +0000

    Working basic home page

--- server/express.js
+++ server/express.js
@@ -5,12 +5,27 @@ import cookieParser from 'cookie-parser'
 import compress from 'compression'
 import cors from 'cors'
 import helmet from 'helmet'
+// modules for server side rendering
+import React from 'react'
+import ReactDOMServer from 'react-dom/server'
+import MainRouter from './../client/MainRouter'
+import { StaticRouter } from 'react-router-dom'
+
+import { ServerStyleSheets, ThemeProvider } from '@material-ui/styles'
+import theme from './../client/theme'
 import Template from './../template'
+//comment out before building for production
+import devBundle from './devBundle'
 
 import userRoutes from './routes/user.routes'
 import authRoutes from './routes/auth.routes'
 
+const CURRENT_WORKING_DIR = process.cwd()
 const app = express()
+
+//comment out before building for production
+devBundle.compile(app)
+
 // parse body params and attache them to req.body 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -20,9 +35,11 @@ app.use(compress())
 app.use(helmet())
 // enable CORS - Cross Origin Resource Sharing 
 app.use(cors())
+
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
+
 // make the app use our user routes
 app.use('/', userRoutes)
-
 // add the authentication routes
 app.use('/', authRoutes)
 
@@ -35,7 +52,25 @@ app.use((err, req, res, next) => {
     }
 })
 
-app.get('/', (req, res) => {
-    res.status(200).send(Template())
+app.get('*', (req, res) => {
+    const sheets = new ServerStyleSheets()
+    const context = {}
+    const markup = ReactDOMServer.renderToString(
+        sheets.collect(
+            <StaticRouter location={req.url} context={context}>
+                <ThemeProvider theme={theme}>
+                    <MainRouter />
+                </ThemeProvider>
+            </StaticRouter>
+        )
+    )
+    if (context.url) {
+        return res.redirect(303, context.url)
+    }
+    const css = sheets.toString()
+    res.status(200).send(Template({
+        markup: markup,
+        css: css
+    }))
 })
 export default app
